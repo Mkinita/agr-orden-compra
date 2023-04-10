30581e695f9ee41fc3d85e4a0d571670cc296a6d
unknown <jerez4959@gmail.com>
2023-04-10 10:42:13 -0400

    Mi mensaje de commitsssssss

--- components/ListadoPedidosInforme.js
+++ components/ListadoPedidosInforme.js
@@ -1,7 +1,7 @@
 import {formatiarFecha} from "helpers/fecha"
 import {formatoNumero} from "helpers/formato"
 import useCombustible from '../hooks/useCombustible';
-import {lastFourDigits} from "helpers/mostrarultimosnumero"
+import {lastFourDigits} from "@/helpers/mostrarultimosnumero"
 import Image from "next/image";
 
 
