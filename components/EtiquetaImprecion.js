2ddaaf526fcbf0a5cd65e80ca4f765714045434f
unknown <jerez4959@gmail.com>
2023-04-14 13:31:45 -0400

    Misssss

--- components/EtiquetaImprecion.js
+++ components/EtiquetaImprecion.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import {formatiarFecha} from "helpers/fecha"
 import axios from 'axios';
 import QRGenerator from '../components/QRGenerator';
 import { useRouter } from 'next/router'
 import { toast } from "react-toastify"
+import Link from 'next/link';
+
 
 const Etiquetas = ({orden}) => {
 
 
 
-    const {nombre,id,pedido,cliente} = orden;
+    const {nombre,id,pedido,cliente,fecha} = orden;
 
     const router = useRouter()
 
@@ -41,8 +44,10 @@ const Etiquetas = ({orden}) => {
                   <h3 className='text-xl font-bold'>{oc.detalle}</h3>
                   <p className='text-lg font-bold py-1'>{nombre}</p>
                   <p className='text-lg font-bold py-1'>{cliente}</p>
+                  <p className='text-lg font-bold py-1'>{formatiarFecha(fecha)}</p>
                   <div className='py-2'>
-      <QRGenerator orden={('-Calidad:')+ nombre + ('/')+ oc.detalle +('/Cliente:')+ cliente +('/N°:')+(id)} />
+      {/* <QRGenerator orden={('-Calidad:')+ nombre + ('/')+ oc.detalle +('/Cliente:')+ cliente +('/N°:')+(id)} /> */}
+      <QRGenerator orden={('https://orden-compra-socal-production.up.railway.app/etiqueta/')+ ('/')+(id)} />
       <p className='text-sm font-bold py-1'>N°: {id}</p>
       <p className="text-sm text-gray-700 mt-2 font-bold">Volumen</p>
       <p className="text-sm text-gray-700 mt-2 font-bold">{oc.espesor * oc.ancho * oc.largo * oc.piezas *oc.cantidad / 1000000 }</p>
