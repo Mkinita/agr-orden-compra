d2210262337a94c858cc787bfb493e56abcf6a10
unknown <jerez4959@gmail.com>
2023-05-16 01:13:09 -0400

    agrwisaaaa

--- components/ListadoSaldosFecha.js
+++ components/ListadoSaldosFecha.js
@@ -12,12 +12,12 @@ const ListadoSaldosFecha = ({saldo}) => {
         <table className="table-auto w-full text-center bg-white text-gray-700">
             <tbody>
 
-                    <tr>
-                        <td className="border px-4 py-2">{formatiarFecha(fecha)}</td>
-                        <td className="border px-4 py-2">{espesor}x{ancho}x{largo}</td>
-                        <td className="border px-4 py-2">1</td>
-                        <td className="border px-4 py-2">{calidad}</td>
-                        <td className="border px-4 py-2">{formatoNumero(espesor * ancho * largo * piezas *1 / 1000000 )}</td>
+                    <tr className="bg-white border-b hover:bg-amber-300 text-sm">
+                        <td className="px-6 py-4 w-1/5 text-center border border-amber-400">{formatiarFecha(fecha)}</td>
+                        <td className="px-6 py-4 w-1/5 text-center border border-amber-400">{espesor}x{ancho}x{largo}</td>
+                        <td className="px-6 py-4 w-1/5 text-center border border-amber-400">1</td>
+                        <td className="px-6 py-4 w-1/5 text-center border border-amber-400">{calidad}</td>
+                        <td className="px-6 py-4 w-1/5 text-center border border-amber-400">{formatoNumero(espesor * ancho * largo * piezas *1 / 1000000 )}</td>
                     </tr>
 
             </tbody>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -181,6 +181,15 @@ const Sidebar = () => {
                         </Link>
                     </li>
 
+                    <li>
+                        <Link href="/listado-produciones-fecha-emp" class="flex items-center p-2 text-base font-black text-black rounded-lg bg-amber-400 dark:text-black hover:bg-gray-100 dark:hover:bg-amber-400 uppercase">
+                            📅
+                            <span class="ml-3">Producciones</span>
+                        </Link>
+
+                        
+                    </li>
+
 
                     
                     <li>
