e7b3cf69342296beadf28ba04b98cbb9dd2ea5b2
unknown <jerez4959@gmail.com>
2023-10-17 12:25:30 -0300

    000123

--- pages/editar-orden.js
+++ pages/editar-orden.js
@@ -2,7 +2,8 @@ import useSWR from 'swr'
 import axios from 'axios'
 import LayoutOrdenCompraSolicitud from "../layout/LayoutOrdenCompraSolicitud"
 import EditarSolicitudNueva from '../components/EditarSolicitudNueva'
-import {useState, useEffect, useCallback} from 'react'
+import {useState, useEffect} from 'react'
+import { useRouter } from 'next/router';
 
 
 
@@ -10,6 +11,48 @@ export default function Admin() {
 
     const fetcher = () => axios('/api/cotizar').then(datos => datos.data)
     const { data, error, isLoading } = useSWR('/api/cotizar',fetcher,{refreshInterval: 100} )
+
+
+
+    const [users, setUsers] = useState([]);
+const [search, setSearch] = useState("");
+const router = useRouter();
+
+const URL = '/api/cotizar';
+
+const showData = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  setUsers(data);
+};
+
+const searcher = (e) => {
+  setSearch(e.target.value);
+};
+
+const results = !search
+  ? users
+  : users.filter(dato => String(dato.id).toLowerCase().includes(search.toLowerCase()));
+
+useEffect(() => {
+  // Intenta obtener el valor de búsqueda del localStorage al cargar la página
+  const storedSearch = localStorage.getItem('searchValue');
+  if (storedSearch) {
+    setSearch(storedSearch);
+  }
+  showData();
+}, []);
+
+useEffect(() => {
+  localStorage.setItem('searchValue', search);
+}, [search]);
+
+// Almacenar y recuperar valor de búsqueda en URL
+useEffect(() => {
+  if (search) {
+    
+  }
+}, [search, router]);
 
     
 
@@ -21,7 +64,11 @@ export default function Admin() {
             <div className='grid gap-4 grid-cols-1 md:grid-cols-1 2xl:grid-cols-1 shadow px-2 rounded-lg'>
             
                 <header class="mb-3 text-1xl font-semibold text-center p-2">Editar Detalle</header>
-                {data && data.length ? data.map(solicitud =>
+                <div className='mt-auto'>
+                <input value={search} onChange={searcher} type="text" placeholder='Buscar' className='text-gray-700 my-5 text-center m-auto flex-wrap-reverse border-yellow-400'/> 🔍
+            </div>
+                {data && data.length ? results.map(solicitud =>
+
                 <div className=''>
                         
                     <EditarSolicitudNueva
