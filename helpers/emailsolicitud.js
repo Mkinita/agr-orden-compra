c6afdcf88d2f9f5418295ff2c719b29a7c34fe9f
CARLOS JEREZ FLORES <68791503+Mkinita@users.noreply.github.com>
2026-04-07 14:09:28 -0400

    Update emailsolicitud.js

--- helpers/emailsolicitud.js
+++ helpers/emailsolicitud.js
@@ -2,22 +2,21 @@ const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const recipients = ['comprasagrifor@gmail.com'];
+const recipients = ['jerez4959@gmail.com'];
 
-// Función para enviar correo electrónico
-async function sendEmail({ to, subject, text }) {
+async function sendEmail({ subject, text }) {
   try {
     const response = await resend.emails.send({
-      from: 'Sistema Agrifor <notificaciones@agrifor.cl>', // 👈 usa tu dominio verificado
+      from: 'onboarding@resend.dev',
       to: recipients,
-      subject: subject || 'Notificación',
-      text: text || 'Tienes una nueva notificación en el sistema.',
+      subject,
+      text,
     });
 
     console.log('Correo enviado:', response);
     return response;
   } catch (error) {
-    console.error('Error al enviar el correo electrónico:', error);
+    console.error('Error al enviar correo:', error);
     throw error;
   }
 }
