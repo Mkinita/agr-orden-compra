fac6aa033be4fe7c28e5be8954e4ae74fbabdf4b
CARLOS JEREZ FLORES <68791503+Mkinita@users.noreply.github.com>
2026-04-07 14:02:28 -0400

    Update edwedwefmailautorizacion.js

--- helpers/emailautorizacion.js
+++ helpers/emailautorizacion.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const recipients = ['jerez4959@gmail.com'];
+const recipients = ['ugavino@gmail.com'];
 
 async function sendEmail({ subject, text }) {
   try {
